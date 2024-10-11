6486e329c17539b198e6c24133223b274bb897b5
Jonathan Borja <jborja.one@gmail.com>
2024-10-10 20:37:21 -0600

    completed exercise

--- IterationStatements/Program.cs
+++ IterationStatements/Program.cs
@@ -12,33 +12,34 @@ namespace IterationStatements
 
                 //Create a List called "numbers" - DONE!
                 var numbers = new List<int>();
-            
+
             //-----START HERE------------------------------------------
             //Create a variable of type int and name it num
             //initialize the variable with a value of 0
 
-
+            int num = 0;
 
             // Create a do-while loop and use the template below:
 
-            {
+            do {
                 // Increment num by 1
-
+                num++;
                 // Then add num to the collection - numbers
                     // Hint: reference num inside of the Add method's parentheses
-                numbers.Add(/* num */);
+                numbers.Add(num);
 
-            } // <---- While your variable is less than 100
+            } while (num < 100);// <---- While your variable is less than 100
 
 
 
             // Create a while loop
             // <--- While num is less than 200
-            {
+            while (num < 200){
                 // Increment num by 1
                 // Then add num to the collection numbers
                     //HINT: copy how this was done in the do while loop
-
+                num++;
+                numbers.Add(num);
             }
 
 
@@ -48,8 +49,10 @@ namespace IterationStatements
 
             // Create a foreach loop using the collection - numbers
                 //In the scope of the foreach loop, print each number in numbers
-             
 
+            foreach(var number in numbers){
+                Console.Write(number);
+            }
 
             Console.WriteLine("");
             Console.WriteLine("Decrease:");
@@ -62,8 +65,9 @@ namespace IterationStatements
                 // Decrement i by 1
 
             //start for loop here
-            {
+            for(var i = 199; i <= numbers.Count; i--){
                 // place numbers[i] inside of the Console.WriteLine() method
+                Console.WriteLine(numbers[i]);
             }
 
             //------------End of exercise
